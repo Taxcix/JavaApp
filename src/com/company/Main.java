939503197a80d22af34fda717b23eb4bc0400a45
taxcix <taxcix@hotmail.com>
2021-12-17 23:57:42 +0200

    Commit: Simples classes. Switch. Test pass

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int num =23;
+        switch (num){
+            case 12:
+                System.out.print("Num is 12");
+                break;
+            case 545:
+                System.out.print("Num is 545");
+                break;
+            case 23:
+                System.out.println("Num is 23");
+                if (num == 23)
+                    System.out.println("All is ok!");
+                break;
+        }
+    }
+}
